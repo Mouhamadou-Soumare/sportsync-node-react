3b4764473b6e936a432cd260b3d8e94257b4493a
Mouhamadou Soumare <mouhamadou-soumare@hotmail.com>
2025-04-22 16:24:25 +0200

    updt cors

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,24 @@
-import express from 'express';
+import express from "express";
 
-import cors from 'cors';
+import cors from "cors";
 
-import {
+import { Resend } from "resend";
 
-    Resend
+import liveMatchesRouter from "./routes/apifootball.js";
 
-} from 'resend';
+import authRouter from "./routes/authentication.js";
 
-import liveMatchesRouter from './routes/apifootball.js';
-
-import authRouter from './routes/authentication.js';
-
-import * as newsRouter from './routes/jsonactus.js';
+import * as newsRouter from "./routes/jsonactus.js";
 
 import path from "path";
 
-import db from './db.js';
+import db from "./db.js";
 
-import {
-
-    fileURLToPath
-
-} from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-
-
 
 const app = express();
 const PORT = 3000;
@@ -36,115 +26,70 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const router = express.Router();
 
-
-
-
 const resendKey = process.env.RESEND_KEY;
 
+app.use("/footballapi", liveMatchesRouter);
 
-app.use('/footballapi', liveMatchesRouter);
+app.use("/auth", authRouter);
 
-app.use('/auth', authRouter);
-
-app.use('/news', newsRouter.default);
-
-
-
-
-
-
-
-
+app.use("/news", newsRouter.default);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send("An error occurred");
+  console.error(err);
+  res.status(500).send("An error occurred");
 });
 
-const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 const resend = new Resend(resendKey);
 
-app.post('/contact', async (req, res) => {
+app.post("/contact", async (req, res) => {
+  try {
+    const {
+      from,
 
-    try {
+      to,
 
-        const {
+      subject,
 
-            from,
+      html,
+    } = req.body;
 
-            to,
-
-            subject,
-
-            html
-
-        } = req.body;
-
-
-
-
-        if (!from || !to || !subject || !html) {
-
-            return res.status(400).json({
-
-                error: 'Missing required fields'
-
-            });
-
-        }
-
-
-
-
-        await resend.emails.send({
-            from: "onboarding@resend.dev",
-            to: to,
-            subject: subject,
-
-            html: html,
-
-        });
-
-
-
-
-        res.status(201).json({
-
-            message: 'Email sent successfully'
-
-        });
-
-    } catch (error) {
-
-        console.error('Error sending contact email:', error);
-
-        res.status(500).json({
-
-            error: 'Internal Server Error'
-
-        });
-
+    if (!from || !to || !subject || !html) {
+      return res.status(400).json({
+        error: "Missing required fields",
+      });
     }
 
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: to,
+      subject: subject,
+
+      html: html,
+    });
+
+    res.status(201).json({
+      message: "Email sent successfully",
+    });
+  } catch (error) {
+    console.error("Error sending contact email:", error);
+
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 });
 
+app.use("/", router);
 
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
 
-
-app.use('/', router);
-
-
-
-
-app.listen(PORT, function(err) {
-
-    if (err) console.log(err);
-
-    console.log("Server listening on PORT", PORT);
-
+  console.log("Server listening on PORT", PORT);
 });
